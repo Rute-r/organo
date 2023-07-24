61dc92b4c8ddee146c0302188523bc2c0fbec01f
Rute-r <rute_ke@hotmail.com>
2023-07-24 15:29:00 -0300

    Elevando a lista de times

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'Ux e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    },
+  ];
+
   const [colaboradores, setColaborador] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -15,9 +53,15 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={(colaborador) => aoNovoColaboradorAdicionado(colaborador)} />
-      <Time nome="Programação" />
-      <Time nome="Frontend" />
-      <Time nome="Data Science" />
+
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
